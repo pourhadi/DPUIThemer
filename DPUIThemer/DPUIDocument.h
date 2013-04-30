5034e37476f4bf0c420917287915d96b0ba54d6b
Daniel Pourhadi <dpourhad@nerdery.com>
2013-04-30 14:58:12 -0500

    Initial commit

---
+++ DPUIThemer/DPUIDocument.h
@@ -0,0 +1,13 @@
+//
+//  DPUIDocument.h
+//  DPUIThemer
+//
+//  Created by Daniel Pourhadi on 4/30/13.
+//  Copyright (c) 2013 Daniel Pourhadi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DPUIDocument : NSDocument
+
+@end
